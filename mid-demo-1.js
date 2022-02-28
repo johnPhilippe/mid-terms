cdfe14aa564715b74a21d8291b640d82617193cd
johnPhilippe <99706118+johnPhilippe@users.noreply.github.com>
2022-02-28 16:15:54 +0800

    Add files via upload

---
+++ mid-demo-1.js
@@ -0,0 +1,28 @@
+//Activity Name: mid-demo-1.js
+//Author Name: Conanan, John Philippe D.
+//Section and Code: wd-202 / 2083
+
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res){
+  res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.get('/process_get', function (req, res){
+  response = {
+    first_name:req.query.first_name,
+    last_name:req.query.last_name
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+})
+
+var server = app.listen(5000, function(){
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+})
---
+++ mid-demo-3.js
@@ -0,0 +1,36 @@
+//Activity Name: mid-demo-3.js
+//Author Name: Conanan, John Philippe D.
+//Section and Code: wd-202 / 2083
+
+var express = require("express");
+var multer = require('multer'); //middleware
+
+var app = express();
+
+var storage = multer.diskStorage({
+  destination: function(req, file, callback){
+    callback(null, './uploads');
+  },
+  filename: function(req, file, callback){
+    callback(null, file.originalname);
+  }
+});
+
+var upload = multer({storage : storage}).single('myfile');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/uploaded', function(req,res){
+  upload(req,res,function(err) {
+    if(err) {
+      return res.end("Error uploading file.")
+    }
+    res.end("File is uploaded successfully!");
+  });
+});
+
+app.listen(2000,function(){
+  console.log("Server is running on port 2000");
+});
